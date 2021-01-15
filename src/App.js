d1cd3d089e1c122c1f3bab4312554c573d9bbf34
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2021-01-15 22:50:04 +0100

    first go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { URL } from './config';
+
+
+function Article({ article }) {
+
+  const {
+    title,
+    url,
+    comments_count,
+    cover_image
+  } = article;
+
+
+
+  return (
+    <div className="bg-gray-900 rounded-xl shadow-lg text-white mb-2.5">
+      <a target="_blank" rel="noreferrer" href={url}>
+        <img className="transition rounded-xl p-1.5 transform -translate-y-2.5 hover:-translate-y-4" src={cover_image} alt={title} />
+      </a>
+      <div className="p-4 transform -translate-y-2.5">
+        <a target="_blank" href={url}>
+          <h2 className="text-2xl">{title}</h2>
+        </a>
+        <p>Comments: {comments_count}</p>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [data, setData] = useState({ articles: [] });
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        setIsLoading(true)
+        const response = await fetch(URL)
+        const data = await response.json()
+        setData({ articles: data })
+      } catch (err) {
+        console.log(err);
+      }
+      setIsLoading(false);
+    }
+    fetchArticles();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800">
+      <div className="container mx-auto px-4">
+        <h1 className="text-white">Scotch.io Card Copy</h1>
+        <p className="text-gray-200">With cards from the dev.to articles api.</p>
+        <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 mt-3.5">
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+              data.articles.map(article => <Article article={article} />)
+            )}
+        </div>
+      </div>
     </div>
   );
 }
